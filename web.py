c34352fd5eaabea2e7ce6626fcbb24ee11b50771
Agnieszka <agamjp@gmail.com>
2024-05-01 14:46:00 +0200

    Add new todo feature

--- web.py
+++ web.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from modules import dict, file_functions as ff
 
+
 todolist = ff.get_todos()
+def add_todo(todos=todolist):
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    ff.write_todos(todos)
+    st.session_state["new_todo"] = ''
+
 
 st.title("My Todo App")
 st.subheader("My Todo App, just smaller")
@@ -10,4 +17,5 @@ st.write("And with teeny tiny letters for people with good eyes!!!")
 for index, item in enumerate(todolist):
     st.checkbox(item, key=index)
 
-st.text_input(label="", placeholder=dict.todo_prompt)
+st.text_input(label="", placeholder=dict.todo_prompt,
+              key="new_todo", on_change=add_todo)
