548e2a0ffb06147f4b81ef7bbcaa8d7cf3771241
Agnieszka <agamjp@gmail.com>
2024-05-01 15:02:05 +0200

    "Complete" feature

--- web.py
+++ web.py
@@ -10,12 +10,19 @@ def add_todo(todos=todolist):
     st.session_state["new_todo"] = ''
 
 
-st.title("My Todo App")
-st.subheader("My Todo App, just smaller")
+st.title("My Todo App title")
+st.subheader("My Todo App title, just smaller")
 st.write("And with teeny tiny letters for people with good eyes!!!")
 
 for index, item in enumerate(todolist):
-    st.checkbox(item, key=index)
+    checkbox = st.checkbox(item, key=index)
+    if checkbox:
+        print(checkbox)
+        print(item)
+        todolist.pop(index)
+        ff.write_todos(todolist)
+        del st.session_state[index]
+        st.rerun()
 
 st.text_input(label="", placeholder=dict.todo_prompt,
               key="new_todo", on_change=add_todo)
